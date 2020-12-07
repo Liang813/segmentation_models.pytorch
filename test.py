cad55b1d9c4f3524fc8a16fb35225075cf8763a4
Liang813 <62755766+Liang813@users.noreply.github.com>
2020-12-07 09:01:24 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
 
+import torch
+
+aa = torch.zeros((1, 12))
+aa[0][4] = 1
+print(aa)
+
+bb = torch.zeros((1,12))
+bb[0][3]=1
+print(bb)
+
+print(torch.sum(aa==bb))
+print(torch.sum(aa==bb) / aa.view(-1).shape[0])
+print(torch.sum(aa==bb, dtype=aa.dtype) / aa.view(-1).shape[0])
